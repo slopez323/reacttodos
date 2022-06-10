67042cf1037217007ce131440c864ee6cae6fa5b
Sarah Lopez <slopez323@gmail.com>
2022-06-10 15:09:35 -0400

    stretch

--- src/App.js
+++ src/App.js
@@ -1,72 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+// import logo from './logo.svg';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [importance, setImportance] = useState("Low");
+  const [toDoList, updateToDoList] = useState([]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ToDoComponent />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <ToDoComponent
+          title={title}
+          setTitle={setTitle}
+          description={description}
+          setDescription={setDescription}
+          importance={importance}
+          setImportance={setImportance}
+          toDoList={toDoList}
+          updateToDoList={updateToDoList}
+        />
       </header>
     </div>
   );
 }
 
-const ToDoComponent = () => {
-  const [title, setTitle] = useState('Title');
-  const [description, setDescription] = useState('Description');
-  const [importance, setImportance] = useState('low');
-  const [toDoList, updateToDoList] = useState([]);
+const importanceOptions = [
+  {
+    title: "Low",
+    value: "Low",
+  },
+  {
+    title: "Medium",
+    value: "Medium",
+  },
+  {
+    title: "High",
+    value: "High",
+  },
+  {
+    title: "Critical",
+    value: "Critical",
+  },
+];
 
+const ToDoList = (props) => {
   return (
     <div>
-      <input type='text' value={title} onChange={(e) => {
-        const newTitle = e.target.value;
-        setTitle(newTitle);
-      }} ></input>
-      <input type='text' value={description} onChange={(e) => {
-        const newDescription = e.target.value;
-        setDescription(newDescription);
-      }}></input>
-      <select onChange={(e) => {
-        const newImportance = e.target.value;
-        setImportance(newImportance);
-      }}>
-        <option value='Low'>Low</option>
-        <option value='Medium'>Medium</option>
-        <option value='High'>High</option>
+      <p>
+        {props.index + 1}. {props.title}
+      </p>
+      <p> Description: {props.description}</p>
+      <p> Importance: {props.importance}</p>
+      <hr />
+    </div>
+  );
+};
+
+const SelectOptions = (props) => {
+  return <option value={props.value}>{props.title}</option>;
+};
+
+const ToDoComponent = (props) => {
+  return (
+    <div>
+      <h1> To-Dos </h1>
+      <input
+        type="text"
+        value={props.title}
+        placeholder="Title"
+        onChange={(e) => {
+          const newTitle = e.target.value;
+          props.setTitle(newTitle);
+        }}
+      ></input>
+      <input
+        type="text"
+        value={props.description}
+        placeholder="Description"
+        onChange={(e) => {
+          const newDescription = e.target.value;
+          props.setDescription(newDescription);
+        }}
+      ></input>
+      <select
+        onChange={(e) => {
+          const newImportance = e.target.value;
+          props.setImportance(newImportance);
+        }}
+      >
+        {importanceOptions.map((option) => {
+          return <SelectOptions title={option.title} value={option.value} />;
+        })}
       </select>
-      <button onClick={(e) => {
-        const newToDo = { title, description, importance };
-        updateToDoList([...toDoList, newToDo]);
-      }}>Submit</button>
+      <button
+        onClick={() => {
+          const newToDo = {
+            title: props.title,
+            description: props.description,
+            importance: props.importance,
+          };
+          props.updateToDoList([...props.toDoList, newToDo]);
+          props.setTitle("");
+          props.setDescription("");
+        }}
+      >
+        Submit
+      </button>
       <hr />
       <div>
-        {toDoList.map((element, index) => {
+        {props.toDoList.map((element, index) => {
           return (
-            <div>
-              <p> {index + 1}. {element.title}</p>
-              <p> Description: {element.description}</p>
-              <p> Importance: {element.importance}</p>
-              <hr />
-            </div>
-          )
+            <ToDoList
+              title={element.title}
+              description={element.description}
+              importance={element.importance}
+              index={index}
+            />
+          );
         })}
       </div>
     </div>
-  )
+  );
 };
 
 export default App;
